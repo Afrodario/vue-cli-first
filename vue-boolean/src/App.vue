93bc3472cfab2962937a0af8edd11c7cb31c9f56
Dario La Tegola <dariolategola@gmail.com>
2022-02-18 16:20:52 +0100

    App Vue

---
+++ vue-boolean/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NewTitle></NewTitle>
+  </div>
+</template>
+
+<script>
+import NewTitle from './components/NewTitle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NewTitle
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-boolean/src/components/NewTitle.vue
@@ -0,0 +1,20 @@
+<template>
+    
+    <div>
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    h1 {
+        color: red;
+        text-transform: uppercase;
+        border: 1px solid black;
+        padding: 10px;
+    }
+</style>
